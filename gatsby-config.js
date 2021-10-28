ad19ae5bfc9c2313eb7b22039ae90c53be943bf6
brunosj <bpstjacques@gmail.com>
2021-10-28 13:37:27 +0200

    disable plugin-offline and remove service-worker

--- gatsby-config.js
+++ gatsby-config.js
@@ -82,7 +82,10 @@ const plugins = [
   },
   // this (optional) plugin enables Progressive Web App + Offline functionality
   // To learn more, visit: https://gatsby.dev/offline
-  `gatsby-plugin-offline`,
+  // `gatsby-plugin-offline`,
+
+  // this removes the service worker installed by gatsby-plugin-offline
+  `gatsby-plugin-remove-serviceworker`,
 ]
 
 // module.exports = client.getEntries().then(() => {
