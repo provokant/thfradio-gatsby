6bd5fca629616fba07d501827e29d2c06ce7859a
Ziad Chaoui <ziad@Ziads-Air.fritz.box>
2022-04-25 11:32:39 +0200

    revert all changes to ticker

--- src/components/live-ticker/index.js
+++ src/components/live-ticker/index.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react"
+import React from "react"
 import moment from "moment"
 import localization from "moment/locale/de"
 import Marquee from "react-marquee-double"
@@ -12,45 +12,37 @@ moment.updateLocale("de", localization)
 export const LiveTicker = () => {
   const { nowPlaying, nextPlaying } = useAllShows()
 
-  const message = (now, next) => now ? `
+  const message = nowPlaying ? `
       LIVE FROM AIRPORT BERLIN <b><i>NOW:&nbsp;&nbsp;&nbsp;&nbsp;</i></b>
-      <i>${now.summary}
-      ${moment(now.start).format("HH:mm")}-${moment(now.end).format("HH:mm")}
+      <i>${nowPlaying.summary}
+      ${moment(nowPlaying.start).format("HH:mm")}-${moment(nowPlaying.end).format("HH:mm")}
       &nbsp;&nbsp;&nbsp;&nbsp;
-      <b>Next show:</b>
-      ${next.summary}
-      ${moment(next.start).format("HH:mm")}-${moment(next.end).format("HH:mm")}
+      <b>next show:</b>
+      ${nextPlaying.summary}
+      ${moment(nextPlaying.start).format("HH:mm")}-${moment(nextPlaying.end).format("HH:mm")}
       </i>
     ` : `
-      <b>NOW PLAYING THF RADIO ARCHIVE</b> – <b>Next show</b>
+      <b>NOW PLAYING THF RADIO ARCHIVE</b> – <b>next show</b>
       on
-      ${moment(next.start).format("dddd, HH:mm")}
-      <i>${next.summary}</i>
+      ${moment(nextPlaying.start).format("dddd, HH:mm")}
+      <i>${nextPlaying.summary}</i>
       –
     `
-    const ticker = message => (
-      <div className="live-ticker">
-        <Marquee
-          step={1}
-          space={100}
-          interval={20}
-          autoStart={true}
-          direction={"left"}
-          delay={1000}
-        >
-          <span dangerouslySetInnerHTML={{ __html: message }} />
-        </Marquee>
-      </div>
-    )
 
-
-  const [tickerDiv, setTickerDiv] = useState(ticker(message(nowPlaying,nextPlaying)))
-    
-  useEffect(()=>{setTickerDiv(ticker(message(nowPlaying,nextPlaying)))}, [nowPlaying,nextPlaying])
-  
-  console.log(tickerDiv)
-
-  return tickerDiv
+  return (
+    <div className="live-ticker">
+      <Marquee
+        step={1}
+        space={100}
+        interval={20}
+        autoStart={true}
+        direction={"left"}
+        delay={1000}
+      >
+        <span dangerouslySetInnerHTML={{ __html: message }} />
+      </Marquee>
+    </div>
+  )
 }
 
 export default LiveTicker
